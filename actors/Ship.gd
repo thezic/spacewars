897ce793da38ce2d2be30e424aabc803b456a1f4
Simon Dahlberg <simon.dahlberg@cdab.eu>
2022-02-04 20:25:44 +0200

    Add shield

--- actors/Ship.gd
+++ actors/Ship.gd
@@ -2,27 +2,72 @@ extends RigidBody2D
 
 signal ship_destroyed
 
+var Plasma = preload("res://objects/plasma_sm/PlasmaSm.tscn")
+var Explosion = preload("res://effects/Explosion.tscn")
+
 export var color := Color(255, 0, 0)
 export var engine_thrust := 500
 export var spin_torque := 2000
 export var recoil := -200
+
+export var shield_energy := 100.0
+export var shield_reload := 20.0
+export var shield_deplete := 200.0
 
 enum Rot { LEFT = -1, NONE = 0, RIGHT = 1 }
 
 var thrust := Vector2()
 var rotation_dir: int = Rot.NONE
 
-var Plasma = preload("res://objects/plasma_sm/PlasmaSm.tscn")
-var Explosion = preload("res://effects/Explosion.tscn")
+var is_shielded := false setget _readonly_setter
+var remaining_shield_energy := shield_energy
+var shield_depleted := false
+
+
+onready var sprite := $Sprite
+onready var shield_node = $Shield/Node2D
+onready var shield_sprite = $Shield/Node2D/ShieldSprite
+onready var shield_energy_meter = $Shield/Node2D/ShieldEnergyMeter
+onready var shield_collider = $ShieldCollisionShape
+
+
+func _readonly_setter(_value):
+	pass
 
 
 func _ready():
-	$Sprite.modulate = color
+	sprite.modulate = color
+	set_shielded(false)
+
 
 func start(pos: Vector2):
 	position = pos
 
+
+func set_shielded(value: bool) -> void:
+	if value == is_shielded:
+		return
+
+	is_shielded = value
+
+	shield_sprite.visible = is_shielded
+	shield_collider.disabled = !is_shielded
+
+
+func try_activate_shield():
+	if shield_depleted:
+		return
+	set_shielded(true)
+
+
+func deactivate_shield():
+	set_shielded(false)
+
+
 func hit(_damage, _normal: Vector2):
+	if is_shielded:
+		return
+
 	queue_free()
 	var explosion = Explosion.instance()
 	explosion.start(position, Vector2())
@@ -31,7 +76,13 @@ func hit(_damage, _normal: Vector2):
 	emit_signal("ship_destroyed")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta):
+	if Input.is_action_just_pressed("player_1_shield"):
+		try_activate_shield()
+
+	if Input.is_action_just_released("player_1_shield"):
+		deactivate_shield()
+
 	if Input.is_action_pressed("player_1_thrust"):
 		thrust = transform.x * engine_thrust
 	elif Input.is_action_pressed("player_1_brake"):
@@ -56,6 +107,21 @@ func _process(_delta):
 		$Particles2D.emitting = true
 	else:
 		$Particles2D.emitting = false
+
+	shield_node.position = position
+
+	if is_shielded:
+		remaining_shield_energy -= delta * shield_deplete
+		shield_energy_meter.value = 100 * remaining_shield_energy / shield_energy
+	elif remaining_shield_energy < shield_energy:
+		remaining_shield_energy += delta * shield_reload
+		shield_energy_meter.value = 100 * remaining_shield_energy / shield_energy
+	else:
+		shield_depleted = false
+
+	if remaining_shield_energy <= 0:
+		shield_depleted = true
+		set_shielded(false)
 
 
 func _physics_process(_delta):
